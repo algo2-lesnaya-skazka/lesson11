791a425bc5f9efe8ddbe34b07f92add891807c4e
givichivi <56928829+givichivi@users.noreply.github.com>
2020-02-12 18:57:24 +0300

    Add files via upload

---
+++ lesson11_1.py
@@ -0,0 +1,3 @@
+Names = ['Simon', 'Kate','Vanya']
+for item in Names:
+    print('Hello', item)
---
+++ lesson11_4.py
@@ -0,0 +1,7 @@
+suitcase= [['hat','tie','sock'],['bag','shoe','shirt']]
+print (suitcase)
+print (suitcase[1])
+print (suitcase[1] [2])
+
+
+
